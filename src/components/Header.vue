16514a89d7bd5b22a220cd056ed241e027f4cccf
jinxing-star <jinxing97@126.com>
2021-06-06 21:45:36 +0800

    社区

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <img src="../assets/header.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    img {
+        width:1800px;
+        height: 50px;
+    }
+
+
+
+
+</style>
